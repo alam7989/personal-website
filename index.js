cb5f7011133fe705b0aed216f6dcd38dbf5e7e89
Audrey Lam <audreylam989@gmail.com>
2023-10-11 22:47:13 -0700

    added 3 placeholder project cards (in a row, click to expand)

---
+++ index.js
@@ -0,0 +1,29 @@
+document.getElementById("beepbeep_button").addEventListener("click", function () {
+    const button = this;
+
+    if (button.textContent === "More Info!") {
+        button.textContent = "Less Info";
+    } else {
+        button.textContent = "More Info!";
+    }
+});
+
+document.getElementById("beijing_gourmet_button").addEventListener("click", function () {
+    const button = this;
+
+    if (button.textContent === "More Info!") {
+        button.textContent = "Less Info";
+    } else {
+        button.textContent = "More Info!";
+    }
+});
+
+document.getElementById("zotdots_button").addEventListener("click", function () {
+    const button = this;
+
+    if (button.textContent === "More Info!") {
+        button.textContent = "Less Info";
+    } else {
+        button.textContent = "More Info!";
+    }
+});
