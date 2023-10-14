808ad016c6b46e7948d00481829054077b51dfbd
Audrey Lam <audreylam989@gmail.com>
2023-10-14 01:38:24 -0700

    made functional project cards that flip on hover...need to get better images for cards

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-document.getElementById("beepbeep_button").addEventListener("click", function () {
-    const button = this;
+// document.getElementById("beepbeep_button").addEventListener("click", function () {
+//     const button = this;
 
-    if (button.textContent === "More Info!") {
-        button.textContent = "Less Info";
-    } else {
-        button.textContent = "More Info!";
-    }
-});
+//     if (button.textContent === "More Info!") {
+//         button.textContent = "Less Info";
+//     } else {
+//         button.textContent = "More Info!";
+//     }
+// });
 
-document.getElementById("beijing_gourmet_button").addEventListener("click", function () {
-    const button = this;
+// document.getElementById("beijing_gourmet_button").addEventListener("click", function () {
+//     const button = this;
 
-    if (button.textContent === "More Info!") {
-        button.textContent = "Less Info";
-    } else {
-        button.textContent = "More Info!";
-    }
-});
+//     if (button.textContent === "More Info!") {
+//         button.textContent = "Less Info";
+//     } else {
+//         button.textContent = "More Info!";
+//     }
+// });
 
-document.getElementById("zotdots_button").addEventListener("click", function () {
-    const button = this;
+// document.getElementById("zotdots_button").addEventListener("click", function () {
+//     const button = this;
 
-    if (button.textContent === "More Info!") {
-        button.textContent = "Less Info";
-    } else {
-        button.textContent = "More Info!";
-    }
-});
+//     if (button.textContent === "More Info!") {
+//         button.textContent = "Less Info";
+//     } else {
+//         button.textContent = "More Info!";
+//     }
+// });
+
+// project card-flipping behavior
